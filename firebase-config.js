406d2099ff57c15aecc7b3a58591d710872fd143
tikusaha <tiku2394@gmail.com>
2026-06-06 05:49:20 +0530

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 window.NEXTGEN_FIREBASE_CONFIG = {
   enabled: true,
   firebaseConfig: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBGAioNpVlz6HaX9B_3Jy4ePWdGtIBEYIc",
+    authDomain: "nextgen-album-billing.firebaseapp.com",
+    projectId: "nextgen-album-billing",
+    storageBucket: "extgen-album-billing.firebasestorage.app",
+    messagingSenderId: "128577468074",
+    appId: "1:128577468074:web:0173d47bda8c86c9a0c2d3"
   }
 };
